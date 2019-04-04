db5791ddb9f132510d54ee3e87012ed8622ecf30
rayndeng <ryan@tonercity.com.au>
2019-04-05 09:29:41 +1100

    typo

--- app/Modules/UI/routes/ui.php
+++ app/Modules/UI/routes/ui.php
@@ -2,12 +2,13 @@
 
 Route::prefix('app')->middleware(['web', 'auth'])->namespace('App\Modules\UI\Controllers')->group(function () {
     Route::get('', 'UIController@index')->name('FrontEnd.index');
+    Route::get('/', function () {
+        return view('ui::index');
+    });
+
+    Route::get('/home', function () {
+        return view('ui::index');
+    });
+
 });
 
-Route::get('/', function () {
-    return view('ui::index');
-});
-
-Route::get('/home', function () {
-    return view('ui::index');
-});
